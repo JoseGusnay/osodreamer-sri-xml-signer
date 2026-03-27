a7704b8bcc5915641db26c35d9eee4106b30203f
Luis Arruda <arruda.lh@gmail.com>
2026-03-27 14:05:34 -0500

    adding Lazzate, corpnewbest and firmasegura strategies based on ANFAC logic

--- src/sign-xml/infrastructure/certificate/factories/sign-strategy.factory.ts
+++ src/sign-xml/infrastructure/certificate/factories/sign-strategy.factory.ts
@@ -6,13 +6,19 @@ import {
   UanatacaStrategy,
 } from "../strategies";
 import { AnfacStrategy } from "../strategies/anfac.strategy";
+import { CorpnewbestStrategy } from "../strategies/corpnewbest.strategy";
+import { FirmaseguraStrategy } from "../strategies/firmasegura.strategy";
+import { LazzateStrategy } from "../strategies/lazzate.strategy";
 
 export class SignStrategyFactory {
   private readonly strategies: SignStrategy[] = [
     new BancoCentralStrategy(),
     new SecurityDataStrategy(),
     new UanatacaStrategy(),
-    new AnfacStrategy()
+    new AnfacStrategy(),
+    new CorpnewbestStrategy(),
+    new FirmaseguraStrategy(),
+    new LazzateStrategy(),
   ];
 
   getStrategy(friendlyName: string): SignStrategy {
--- src/sign-xml/infrastructure/certificate/strategies/index.ts
+++ src/sign-xml/infrastructure/certificate/strategies/index.ts
@@ -1,4 +1,7 @@
 export * from "./anfac.strategy";
 export * from "./banco-central.strategy";
+export * from "./corpnewbest.strategy";
+export * from "./firmasegura.strategy";
+export * from "./lazzate.strategy";
 export * from "./security-data.strategy";
 export * from "./uanataca.strategy";
